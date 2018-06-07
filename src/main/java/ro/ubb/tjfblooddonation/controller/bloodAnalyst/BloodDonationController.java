181a6242a32db7a4892a86ac891e1dea55b070ca
Bianca <nbie2189@scs.ubbcluj.ro>
2018-06-07 15:26:40 +0300

    blood analysis

--- src/main/java/ro/ubb/tjfblooddonation/controller/bloodAnalyst/BloodDonationController.java
+++ src/main/java/ro/ubb/tjfblooddonation/controller/bloodAnalyst/BloodDonationController.java
@@ -45,6 +45,48 @@ import java.util.stream.Collectors;
 
 @Controller
 public class BloodDonationController {
+//    @FXML
+//    private ComboBox<?> bloodComboBox;
+    private Set<BloodComponent> bloodComponents;
+
+    @FXML
+    private RadioButton hepatitisRadioButton;
+
+    @FXML
+    private RadioButton malariaRadioButton;
+
+    @FXML
+    private RadioButton epilepsyRadioButton;
+
+    @FXML
+    private RadioButton mentalRadioButton;
+
+    @FXML
+    private RadioButton ulcerRadioButton;
+
+    @FXML
+    private RadioButton diabetRadioButton;
+
+    @FXML
+    private RadioButton heartRadioButton;
+
+    @FXML
+    private RadioButton skinRadioButton;
+
+    @FXML
+    private RadioButton cancerRadioButton;
+
+    @FXML
+    private RadioButton myopiaRadioButton;
+
+    @FXML
+    private Button analyzeButton;
+
+    @FXML
+    void analyzeButtonClicked(ActionEvent event) {
+
+    }
+
     private static final SpringFxmlLoader loader = new SpringFxmlLoader();
     @Autowired
     BloodService bloodService;
@@ -55,7 +97,60 @@ public class BloodDonationController {
 
     private void refresh(){
         Set<Blood> blood = bloodService.getUnanalysedBlood();
+
+        if(hepatitisRadioButton.selectedProperty().get())
+            blood = blood
+                    .stream()
+                    .filter(bc -> bc.getAnalysis().equals("hepatitis"))
+                    .collect(Collectors.toSet());
+        if(malariaRadioButton.selectedProperty().get())
+            blood = blood
+                    .stream()
+                    .filter(bc -> bc.getAnalysis().equals("malaria"))
+                    .collect(Collectors.toSet());
+        if(epilepsyRadioButton.selectedProperty().get())
+            blood = blood
+                    .stream()
+                    .filter(bc -> bc.getAnalysis().equals("epilepsy"))
+                    .collect(Collectors.toSet());
+        if(mentalRadioButton.selectedProperty().get())
+            blood = blood
+                    .stream()
+                    .filter(bc -> bc.getAnalysis().equals("mental disease"))
+                    .collect(Collectors.toSet());
+        if(ulcerRadioButton.selectedProperty().get())
+            blood = blood
+                    .stream()
+                    .filter(bc -> bc.getAnalysis().equals("ulcer"))
+                    .collect(Collectors.toSet());
+        if(diabetRadioButton.selectedProperty().get())
+            blood = blood
+                    .stream()
+                    .filter(bc -> bc.getAnalysis().equals("diabet"))
+                    .collect(Collectors.toSet());
+        if(heartRadioButton.selectedProperty().get())
+            blood = blood
+                    .stream()
+                    .filter(bc -> bc.getAnalysis().equals("heart disease"))
+                    .collect(Collectors.toSet());
+        if(skinRadioButton.selectedProperty().get())
+            blood = blood
+                    .stream()
+                    .filter(bc -> bc.getAnalysis().equals("skin disease"))
+                    .collect(Collectors.toSet());
+        if(cancerRadioButton.selectedProperty().get())
+            blood = blood
+                    .stream()
+                    .filter(bc -> bc.getAnalysis().equals("cancer"))
+                    .collect(Collectors.toSet());
+        if(myopiaRadioButton.selectedProperty().get())
+            blood = blood
+                    .stream()
+                    .filter(bc -> bc.getAnalysis().equals("myopia"))
+                    .collect(Collectors.toSet());
         bloodComboBox.setItems(FXCollections.observableArrayList(blood));
+        System.out.println("Number of diseases is " + blood.size());
+
     }
     public void initialize(){
         refresh();
